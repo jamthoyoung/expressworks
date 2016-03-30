7d6fa4be59fdf369a4cb64eb6b376deae5fb6595
James Young <jamthoyoung@gmail.com>
2016-03-30 11:40:19 -0400

    exercise 4 (doesnt work)

--- program.js
+++ program.js
@@ -1,5 +1,7 @@
 var express = require('express');
-var path = require('path');
+//var path = require('path');
+var bodyparser = require('body-parser')
+
 var app = express();
 // exercise 1
 //app.get('/home', function(req, res) {
@@ -8,10 +10,15 @@ var app = express();
 // exercise 2
 //app.use(express.static(process.argv[3] || path.join(__dirname, 'public')));
 // exercise 3
-app.set('views', path.join(__dirname, 'templates'));
-app.set('view engine', 'jade');
-app.get('/home', function(req, res) {
-  res.render('index', {date: new Date().toDateString()});
-});
+//app.set('views', path.join(__dirname, 'templates'));
+//app.set('view engine', 'jade');
+//app.get('/home', function(req, res) {
+//  res.render('index', {date: new Date().toDateString()});
+//});
 
+//exercise 4
+app.use(bodyparser.urlencoded({extended: true}))
+app.post('/form', function(req, res) {
+  res.end(req.body.str.split('').reverse().join(''));
+});
 app.listen(process.argv[2]);
