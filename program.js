e4891aaf48924991b24442c3ce3bd94748da340f
James Young <jamthoyoung@gmail.com>
2016-04-01 13:14:04 -0400

    move things around

--- program.js
+++ program.js
@@ -8,8 +8,8 @@ var app = express();
 //  res.end('Hello World!');
 //});
 // exercise 2
-app.use(require('stylus').middleware(process.argv[3]))
-app.use(express.static(process.argv[3]));
+//app.use(require('stylus').middleware(process.argv[3]))
+//app.use(express.static(process.argv[3]));
 // exercise 3
 //app.set('views', path.join(__dirname, 'templates'));
 //app.set('view engine', 'jade');
@@ -22,4 +22,11 @@ app.use(express.static(process.argv[3]));
 //app.post('/form', function(req, res) {
 //  res.end(req.body.str.split('').reverse().join(''));
 //});
+//exercise 5
+app.use(require('stylus').middleware(process.argv[3]))
+app.use(express.static(process.argv[3]));
+
+//exercise 6 param
+
+
 app.listen(process.argv[2]);
